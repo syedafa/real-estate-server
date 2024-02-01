5295aeb2ba02ab5e4a8acff5384a64963bc0127a
syedafa <afwaan3@gmail.com>
2024-02-01 11:29:47 +0530

    connected to data base

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to mongo db successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(8080, () => {
   console.log("app is running on port 8080");
 });
